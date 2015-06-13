71b2f200a1b625fc90ba8957f63b3bd3ad44aff6
Kildare Lauser <Kildare Lauser>
2015-06-12 23:02:57 -0430

    fix https

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title><?php bloginfo('name'); ?></title>
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
-	<link href='http://fonts.googleapis.com/css?family=Reenie+Beanie|Lato:400,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Reenie+Beanie|Lato:400,700' rel='stylesheet' type='text/css'>
 	<!-- Flexboxgrid CSS base -->
 	<?php wp_enqueue_style('flexboxgrid', get_template_directory_uri() . '/assets/css/vendor/flexboxgrid.min.css',true); ?>
 	<!-- Estilo principal del sitio -->
